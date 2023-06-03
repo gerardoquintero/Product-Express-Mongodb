9d6e615e07c8f3f7070f19dedd28bc863108afcb
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-06-03 12:41:23 -0600

    REST http route  build

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override')
+
+
+const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -15,11 +20,60 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//creating a basic route 
-app.get('/dog', (req, res) => {
-    res.send('woof!')
-})  
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+app.get('/products', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { categories })
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
+
+
 
 app.listen(3000, () => {
-    console.log('App is listening on port 3000 xd!')
+    console.log("APP IS LISTENING ON PORT 3000!")
 })
+
+
